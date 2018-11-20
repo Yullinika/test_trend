63fc9dce20b56746f859c71368a52aeaf74fa09a
Admin <yunova2552@gmail.com>
2018-11-20 15:18:32 +0300

    Updates

---
+++ precache-manifest.d7d795f0d4ae954abb36e2942b6c6791.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2f88491dbaea71e9894",
+    "url": "/test_trend/static/js/runtime~main.b2f88491.js"
+  },
+  {
+    "revision": "6e92ec53d94d07fc37c4",
+    "url": "/test_trend/static/js/main.6e92ec53.chunk.js"
+  },
+  {
+    "revision": "3608a542ce9a76d2bb34",
+    "url": "/test_trend/static/js/1.3608a542.chunk.js"
+  },
+  {
+    "revision": "6e92ec53d94d07fc37c4",
+    "url": "/test_trend/static/css/main.30bc3453.chunk.css"
+  },
+  {
+    "revision": "5964b2fcc2153459ecd26efbe0ecc224",
+    "url": "/test_trend/index.html"
+  }
+];
